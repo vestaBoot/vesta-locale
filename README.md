9833fe636c8a42533249dc92f000037ce0636d3f
hbtb <bishehsary@gmail.com>
2018-12-12 05:13:21 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# [Vesta](https://vesta.bz) Module template
+
+Collection of [vestaBoot](https://github.com/vestaBoot) Module template
---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const { Indexer, Packager } = require("@vesta/devmaid");
+const gulp = require("gulp")
+
+let pkgr = new Packager({
+    root: __dirname,
+    src: "src",
+    targets: ["es6"],
+    files: [".npmignore", "LICENSE", "README.md"],
+    publish: "--access=public",
+    transform: {
+        package: (json, target) => {
+            delete json.private;
+            return false;
+        },
+        tsconfig: function(tsconfig, target, isProduction) {
+            tsconfig.compilerOptions.target = target;
+        }
+    }
+});
+
+function indexer() {
+    const indexer = new Indexer("src");
+    indexer.generate();
+    return Promise.resolve();
+}
+
+function watch() {
+    gulp.watch(["src/**/*", "!src/**/index.ts"], indexer);
+    return Promise.resolve();
+}
+
+const tasks = pkgr.createTasks();
+module.exports = {
+    default: gulp.series(indexer, tasks.default, watch),
+    publish: gulp.series(indexer, tasks.deploy, tasks.publish)
+}
